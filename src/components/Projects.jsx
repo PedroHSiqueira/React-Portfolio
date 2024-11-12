6a85045bb7a5acfc2478f4209c9bd1380f65be65
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-12 13:52:15 -0300

    Fix: Details

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,13 +13,17 @@ export function Projects() {
 
       <div>
         <article className="projetos-pessoais">
-            <img src={Verbalize} id="projetos" />
-            <div>
+          <img src={Verbalize} id="projetos" />
+          <div>
             <h4>Verbalize 💬</h4>
             <p>{t("verbalize")}</p>
             <div className="midia_buttons">
-              <a href="https://github.com/PedroHSiqueira/Next_Verbalise_Frontend" target="_blank" className="button">Github</a>
-              <a href="https://verbalize-senac.vercel.app/" target="_blank" className="button">Deploy</a>
+              <a href="https://github.com/PedroHSiqueira/Next_Verbalise_Frontend" target="_blank" className="button">
+                Github
+              </a>
+              <a href="https://verbalize-senac.vercel.app/" target="_blank" className="button">
+                Deploy
+              </a>
             </div>
           </div>
         </article>
@@ -27,13 +31,17 @@ export function Projects() {
 
       <div>
         <article className="projetos-pessoais">
-            <img src={CarSale} id="projetos" />
-            <div>
+          <img src={CarSale} id="projetos" />
+          <div>
             <h4>MileStone Motors 🚗</h4>
             <p>{t("milestone_motors")}</p>
             <div className="midia_buttons">
-              <a href="https://github.com/PedroHSiqueira/Car-Sale-React.js" target="_blank" className="button">Github</a>
-              <a href="https://car-sale-react-js.vercel.app/" target="_blank" className="button">Deploy</a>
+              <a href="https://github.com/PedroHSiqueira/Car-Sale-React.js" target="_blank" className="button">
+                Github
+              </a>
+              <a href="https://car-sale-react-js.vercel.app/" target="_blank" className="button">
+                Deploy
+              </a>
             </div>
           </div>
         </article>
@@ -41,27 +49,35 @@ export function Projects() {
 
       <div>
         <article className="projetos-pessoais">
+          <img src={Weather} alt="controle de chamados" id="projetos" />
           <div>
             <h4>Aplicativo de Clima 🌧️</h4>
             <p>{t("weather")}</p>
             <div className="midia_buttons">
-              <a href="https://github.com/PedroHSiqueira/Flutter_Weather_App" target="_blank" className="button">Github</a>
-              <a href="https://github.com/PedroHSiqueira/Flutter_Weather_App" target="_blank" className="button">Deploy</a>
+              <a href="https://github.com/PedroHSiqueira/Flutter_Weather_App" target="_blank" className="button">
+                Github
+              </a>
+              <a href="https://github.com/PedroHSiqueira/Flutter_Weather_App" target="_blank" className="button">
+                Deploy
+              </a>
             </div>
           </div>
-          <img src={Weather} alt="controle de chamados" id="projetos" />
         </article>
       </div>
 
       <div>
         <article className="projetos-pessoais">
-            <img src={Pokemon} id="projetos" />
+          <img src={Pokemon} id="projetos" />
           <div>
             <h4>Pokemon Terminal 🏃‍♂️</h4>
             <p>{t("pokemon")}</p>
             <div className="midia_buttons">
-              <a href="https://github.com/PedroHSiqueira/Pokemon-Terminal" target="_blank" className="button">Github</a>
-              <a href="https://github.com/PedroHSiqueira/Pokemon-Terminal" target="_blank" className="button">Deploy</a>
+              <a href="https://github.com/PedroHSiqueira/Pokemon-Terminal" target="_blank" className="button">
+                Github
+              </a>
+              <a href="https://github.com/PedroHSiqueira/Pokemon-Terminal" target="_blank" className="button">
+                Deploy
+              </a>
             </div>
           </div>
         </article>
